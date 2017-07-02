555c46d1ad73fd5ea47ef918055dc696ae656c2d
Travis Elkins <travis.elkins+github@gmail.com>
2017-07-02 17:44:40 +0200

    Removed some unnecessary functionality/dependencies in the auth controller and updated composer to make sure that (some of) what's used is present.

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -4,16 +4,11 @@ namespace Olymbytes\Z00s\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Olymbytes\Z00s\LoginProxy;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Olymbytes\Z00s\Http\Requests\LoginRequest;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class AuthController extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
 	public $loginProxy;
 
 	public function __construct(LoginProxy $loginProxy)
