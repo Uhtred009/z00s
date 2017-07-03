e7225555e2b5a5f82f9d494fcb41771d294c86c1
Morten Poul Jensen <mpj@foreno.dk>
2017-07-03 08:19:22 +0200

    Add a facade for the login proxy.

---
+++ src/Facades/LoginProxy.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Olymbytes\Z00s\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LoginProxy extends Facade
+{
+	protected static function getFacadeAccessor()
+	{
+		return 'LoginProxy';
+	}
+}
