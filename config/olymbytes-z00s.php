f0edad39245ec7d1c264f2d34349a208bfe365d4
Morten Poul Jensen <mpj@foreno.dk>
2017-07-03 08:43:36 +0200

    Update config with credentials and provider.

--- config/olymbytes-z00s.php
+++ config/olymbytes-z00s.php
@@ -3,8 +3,16 @@
 return [
 
     /*
-     * setting 1...
+     * Credentials for the user based provider
      */
-    'setting' => env('Z00S_SETTING', true),
+    'credentials' => [
+    	'password_client_id' => env('PASSWORD_CLIENT_ID', ''),
+    	'password_client_secret' => env('PASSWORD_CLIENT_SECRET', ''),
+    ],
 
+    /*
+     * Config based provider uses the credentials set in this file.
+     * User based provider fetches the client id and secret from the user. 
+     */
+    'provider' => 'config',
 ];
