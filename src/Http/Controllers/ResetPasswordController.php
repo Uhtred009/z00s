992ee3089cfb13f68be83b9c152e824898dc1b24
Morten Poul Jensen <mpj@signifly.com>
2017-07-28 16:13:22 +0200

    Import Str class.

--- src/Http/Controllers/ResetPasswordController.php
+++ src/Http/Controllers/ResetPasswordController.php
@@ -2,6 +2,7 @@
 
 namespace Olymbytes\Z00s\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Routing\Controller as BaseController;
